f119c30e98b82fbfcdf849ae42537d865c9e556f
kkoujah <90155283+kkoujah@users.noreply.github.com>
2023-04-13 00:55:58 -0700

    Update edit.py

--- edit.py
+++ edit.py
@@ -1,7 +1,2 @@
-function sumThreeNumbers(num1, num2, num3) {
-  let sum = num1 + num2 + num3;
-  return sum;
-}
-
-let totalSum = sumThreeNumbers(2, 5, 8);
-console.log(totalSum); // Output: 15
+def sum(a, b):
+  return a + b
