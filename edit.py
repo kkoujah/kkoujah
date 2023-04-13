719a3ca798f9bcb3f66a87b2c8877ba36ab37430
kkoujah <90155283+kkoujah@users.noreply.github.com>
2023-04-13 01:49:48 -0700

    Update file contents

--- edit.py
+++ edit.py
@@ -1,3 +1,3 @@
-def sum_seven_numbers(num1, num2, num3, num4, num5, num6, num7):
-    total = num1 + num2 + num3 + num4 + num5 + num6 + num7
+def sum_three_numbers(num1, num2, num3):
+    total = num1 + num2 + num3
     return total
