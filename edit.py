d7f24c0646ef11688a6771fd5aa8c4ba136b82c5
kkoujah <90155283+kkoujah@users.noreply.github.com>
2023-04-13 00:53:47 -0700

    Update edit.py

--- edit.py
+++ edit.py
@@ -1,4 +1,2 @@
-
-This function takes two integer arguments `num1` and `num2` and returns their sum by adding them together using the `+` operator. 
-
-To use this function, you can call it with two integer values like this:
+def sum(a, b):
+  return a + b
