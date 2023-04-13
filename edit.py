0581565072495ba97cfac43c11813bd1612469e4
kkoujah <90155283+kkoujah@users.noreply.github.com>
2023-04-13 01:48:09 -0700

    Update file contents

--- edit.py
+++ edit.py
@@ -1,3 +1,3 @@
-def sum_five_numbers(num1, num2, num3, num4, num5):
-    total = num1 + num2 + num3 + num4 + num5
+def sum_seven_numbers(num1, num2, num3, num4, num5, num6, num7):
+    total = num1 + num2 + num3 + num4 + num5 + num6 + num7
     return total
